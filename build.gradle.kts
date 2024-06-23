f860f5de06b1f4389c59d5cdbe33baaeca59cb16
zoomq8 <104871450+zoomq8@users.noreply.github.com>
2024-06-24 01:13:44 +0300

    Update and rename build.gradle to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+version = 7
+
+cloudstream {
+    authors = listOf( "zoomq8" )
+
+	language = "ar"
+	
+    status = 1
+
+    tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "AsianDrama" )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=faselhd.io&sz=%size%"
+}
